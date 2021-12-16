48452baed9d060f6bfeb27398e07b6633669d39d
Phil-Welsh <pwelsh8@gmail.com>
2021-12-16 18:32:18 -0500

    Deleted models and redirects file.

--- recoil/atoms.js
+++
@@ -1,8 +0,0 @@
-import { atom } from "recoil";
-
-// this is going to create a named export
-// called userState for us to be able to import in our component files
-export const userState = atom({
-    key: "userState",
-    default: null,
-});
