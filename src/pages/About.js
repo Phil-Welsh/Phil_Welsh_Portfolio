77f209876cb7f208ff550cd32ade2d2c35964434
Phil-Welsh <pwelsh8@gmail.com>
2022-01-11 11:06:50 -0800

    Switching to imgur link

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ const About = () => {
     return (
         <div className="about-body">
             <h1>About</h1>
-            <img src="https://ucaf1d324979c58dbbea9151d1ee.previews.dropboxusercontent.com/p/thumb/ABY_G-u1JTi9ywra0VRq1o-wLz4lMT8zWtpjNrP6LjgE-mwRzzKWHA1cQO-2aA-rWs0YHHf3IOSESJ6W98OsWGoTtd0Q95AtfLgvjab_DJ9R_VPSoztLpir6yAVMVZOqV8BZ4a6cFzsTCCZOwVm_9NfdmU86wkZZX7k5b7TpAWPbBHTBHNOuX3sBfWueTxxoc1XUPTcgBDkCzA7As6xRnqgxru-6C8Y59pdwP6ak_U1tJ4qr3tg3rEdyDfAKWkN4U_nbtx8qEdO889BdxDXaEwsTFrT3W5bqpNbv_BV3vVMawmSV3KafJj4wSdZhJj397Lrc6joDThY-oqTKjPEj6JN_mC_N2_fGnwITJpVoiDKJqPyUIyuu1oE_exODxGMrhvY/p.jpeg"/>
+            <img src="https://i.imgur.com/qysRAxb.jpeg"/>
             <p>
             I’m a full stack web developer with a background in mapping and
             Geographic Information Systems. Growing up with a fierce curiosity
