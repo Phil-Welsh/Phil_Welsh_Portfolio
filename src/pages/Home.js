000fcdfcf29386accf9341f35295b88f277f5db7
Phil-Welsh <pwelsh8@gmail.com>
2022-01-14 19:35:28 -0800

    Fixing resume link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,11 +6,12 @@ const Home = () => {
   return (
     <div className="home-block">
       <h1>Phil Welsh</h1>
-      <h3>San Diego based web developer</h3>
+      <h4>San Diego based web developer</h4>
+      <br/>
       <div>
         <a href="https://www.linkedin.com/in/phil-welsh/" target="_blank">LinkedIn</a>
         <a href="https://github.com/Phil-Welsh" target="_blank">Github</a>
-        <a href="https://www.dropbox.com/s/t5p193gbgaodd6f/Phil%20Welsh%20-%20Resume.pdf" target="_blank">Resume</a>
+        <a href="https://www.dropbox.com/s/49cbwt8f4mm6nhj/Phil%20Welsh%20-%20Resume%20Jan%2022.pdf?dl=0" target="_blank">Resume</a>
       </div>
     </div>
   );
