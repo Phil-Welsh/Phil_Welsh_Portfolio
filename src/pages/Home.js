f625bb7166c8d835ddb62d8c270041b26bf53cf3
Phil-Welsh <pwelsh8@gmail.com>
2022-01-25 17:47:13 -0800

    Changing resume link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,7 +11,7 @@ const Home = () => {
       <div>
         <a href="https://www.linkedin.com/in/phil-welsh/" target="_blank">LinkedIn</a>
         <a href="https://github.com/Phil-Welsh" target="_blank">Github</a>
-        <a href="https://www.dropbox.com/s/49cbwt8f4mm6nhj/Phil%20Welsh%20-%20Resume%20Jan%2022.pdf?dl=0" target="_blank">Resume</a>
+        <a href="https://www.dropbox.com/s/l6fgdw0kbvhz3xg/Phil%20Welsh%20-%20Resume%201-25-22.pdf?dl=0" target="_blank">Resume</a>
       </div>
     </div>
   );
