a5b2c3341c37db2d0bd14b69cc27d0431800264d
Phil-Welsh <pwelsh8@gmail.com>
2022-02-22 11:21:35 -0800

    Change resume link

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,13 +6,13 @@ const Home = () => {
   return (
     <div className="home-block">
       <h1>Phil Welsh</h1>
-      <p>Full Stack Web Developer - San Diego </p>
+      <p>Full Stack Software Engineer - San Diego </p>
       <img src="https://i.imgur.com/qysRAxb.jpeg"/>
       <br/>
       <div>
         <a href="https://www.linkedin.com/in/phil-welsh/" target="_blank">LinkedIn</a>
         <a href="https://github.com/Phil-Welsh" target="_blank">Github</a>
-        <a href="https://www.dropbox.com/s/l6fgdw0kbvhz3xg/Phil%20Welsh%20-%20Resume%201-25-22.pdf?dl=0" target="_blank">Resume</a>
+        <a href="https://www.dropbox.com/s/ossf3nvhdg09kz2/Phil%20Welsh%20-%20Resume%202-22-22.pdf?dl=0" target="_blank">Resume</a>
       </div>
     </div>
   );
